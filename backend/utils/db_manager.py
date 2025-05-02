b4c6d57028010633c96127eb086d674bd61d4f30
Egor Miroevsky <57674715+k0rdun@users.noreply.github.com>
2025-05-02 18:32:30 +0300

    add user getter and creator for db_manager

--- backend/utils/db_manager.py
+++ backend/utils/db_manager.py
@@ -19,3 +19,52 @@ class DatabaseManager:
         '''Деструктор, закрывающий сессию'''
         self.session.close()
         self.driver.close()
+    
+
+    def get_user(self, login: str) -> dict:
+        '''Получение пользователя по его логину'''
+        user = self.session.execute_read(
+            lambda tx: tx.run(
+                '''MATCH (u:User {login: $login})
+                RETURN u {
+                    .login, .role, .full_name, .age,
+                    .status, .description, .education,
+                    .created_at, .updated_at, .photo_url
+                } AS u''',
+                login=login
+            ).single()
+        )
+        return dict(user['u']) if user else None
+
+    
+    def user_exists(self, login: str) -> bool:
+        '''Проверка, существуют ли пользователь с заданным логином'''
+        return self.get_user(login) is not None
+
+
+    def create_user(self,
+            login: str, password: str, role: str, full_name: str, 
+            age: int, status: str = 'active', description: str = '',
+            education: str = '', photo_url: str = 'no_photo.png'
+    ) -> bool:
+        '''Создание нового пользователя в базе данных'''
+        # Проверка, что пользователя с таким логином не существует
+        if self.user_exists(login):
+            return False
+        # Создание пользователя
+        self.session.execute_write(
+            lambda tx: tx.run(
+                '''CREATE (u:User {
+                        login: $login, password: $password, role: $role, 
+                        full_name: $full_name, age: $age, status: $status, 
+                        created_at: datetime(), updated_at: datetime(),
+                        description: $description, education: $education, 
+                        photo_url: $photo_url
+                    })''',
+                login=login, password=password, role=role, 
+                full_name=full_name, age=age, status=status,
+                description=description, education=education,
+                photo_url=photo_url
+            )
+        )
+        return True
