552701f78a9e0b928ffc6b2c0f697fa39d19a557
Pavel Bobrov <112647982+pashabbrv@users.noreply.github.com>
2025-06-01 05:43:28 +0300

    Update Admin.tsx

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -62,8 +62,8 @@ const Admin = () => {
     <div className="min-h-screen">
       <Header />
       
-      <div className="container mx-auto p-4 hidden">
-        <div className="text-center mb-10">
+      <div className="container mx-auto p-4">
+        <div className="text-center mb-10 hidden">
           <h1 className="text-4xl font-bold mb-2">Панель администратора</h1>
           <h2 className="text-2xl">для работы с БД</h2>
         </div>
